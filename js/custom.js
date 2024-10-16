0ff71195822904161ac0766d29dfe017d9c4b422
WAJDAN-KHAN <ahsan.ali290620@gmail.com>
2024-10-16 14:16:42 +0500

    done changes

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+
+
+    // Store the current scroll position in session storage when the page is unloaded
+window.addEventListener('beforeunload', () => {
+  sessionStorage.setItem('scrollPosition', window.scrollY.toString());
+});
+
+// Check if there's a stored scroll position and restore it
+window.addEventListener('DOMContentLoaded', () => {
+  const storedScrollPosition = sessionStorage.getItem('scrollPosition');
+  if (storedScrollPosition !== null) {
+      window.scrollTo(0, parseInt(storedScrollPosition));
+      sessionStorage.removeItem('scrollPosition');
+  }
+});
+
+
+    $(window).on('scroll', function(){
+      function isScrollIntoView(elem, index) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(window).height()*.5;
+        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
+          $(elem).addClass('active');
+        }
+        if(!(elemBottom <= docViewBottom)) {
+          $(elem).removeClass('active');
+        }
+        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
+        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
+        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
+      }
+      var timeline = $('#vertical-scrollable-timeline li');
+      Array.from(timeline).forEach(isScrollIntoView);
+    });
+  
+  })(window.jQuery);
+
+
